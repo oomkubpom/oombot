ce2cba12854f36aaadd13da5a4822bf6ed8d9b24
oomkubpom <oomkubpom@gmail.com>
2016-04-24 15:39:52 +0700

    edit

--- index.js
+++ index.js
@@ -28,10 +28,9 @@ app.post('/webhook/', function (req, res) {
       // Handle a text message from this sender
       console.log(text);
       if (text === 'hi') {
-        sendTextMessage(sender,"Hello
-        let me introduce myself");
-        sendTextMessage("my name is Pongsathon Bunnuwong or you can call me Oom");
-        sendTextMessage("Do you want to know anything about me ? Education,Address.Type a keyword and enter pls.")
+        sendTextMessage(sender,"Hello let me introduce myself");
+        sendTextMessage(sender"my name is Pongsathon Bunnuwong or you can call me Oom");
+        sendTextMessage(sender"Do you want to know anything about me ? Education,Address.Type a keyword and enter pls.")
       }
       else {
         sendTextMessage(sender, "Just say hi to get started");
