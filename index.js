44fc1b49f93c85d5d748eb898e2fbec2fd2badd0
oomkubpom <oomkubpom@gmail.com>
2016-04-24 14:18:20 +0700

    edit

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token')
 })
 
+app.post('/webhook/', function (req, res) {
+  var messaging_events = req.body.entry[0].messaging
+  for (var i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i]
+    var sender = event.sender.id
+    if (event.message && event.message.text) {
+      var text = event.message.text
+      console.log(text)
+    }
+  }
+  res.sendStatus(200)
+})
+
 app.set('port', (process.env.PORT || 5000))
 
 app.listen(app.get('port'), function () {
