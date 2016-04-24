338563c518f6356d81e85c1c2f9521981388e03e
oomkubpom <oomkubpom@gmail.com>
2016-04-24 14:49:32 +0700

    edit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/webhook/', function (req, res) {
       var text = event.message.text;
       // Handle a text message from this sender
       console.log(text);
-      sendTextMessage(sender, "I don't know"+ text.substring(0, 200));
+      sendTextMessage(sender, "I don't know "+ text.substring(0, 200));
     }
   }
   res.sendStatus(200);
