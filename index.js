5a3aa13f9879030e4f26c6006b45384ee1e17d59
oomkubpom <oomkubpom@gmail.com>
2016-05-29 11:31:02 +0700

    edit

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/webhook/', function (req, res) {
         sendTextMessage(sender, sum/num.length)
       }
       else {
-        sendTextMessage(sender,"pls input sum,max,min and 2 number or input avg and number1 - numberN")
+        sendTextMessage(sender,"pls input keyword sum,max,min and 2 number or avg and number1 - numberN")
       }
     }
   }
